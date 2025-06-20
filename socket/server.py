2849dc44462b1ff23ff1f2c5ee898342e61f569f
Depressilkin <easy_cure7103@mail.ru>
2025-06-20 22:14:07 +0300

    Update server.py

--- socket/server.py
+++ socket/server.py
@@ -6,28 +6,28 @@
 #участника разговора.
 
 import socket
+import threading
 def handle_client(conn, address):
     print(f'Connect client {address}')
-    conn.sendall('Hello!'.encode())
     while True:
         date = conn.recv(1024).decode(encoding='utf-8')
         if date == False:
-            print('Disconnect')
             break
         print(f'Messag from {address}: {date}')
-        response = input('Input response to client')
-        conn.sendall((response + '\n').encode())
-
-
+        for client in clients_list:
+            if client != conn:
+                client.sendall((date + '\n').encode())
 
 host = '127.0.0.1'
 port = 3000
+clients_list = []
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, port))
     print(f'server {host} runer')
-    s.listen(3)
+    s.listen(5)
     while True:
         conn, address = s.accept()
-        handle_client(conn, address)
+        clients_list.append(conn)
+        threading.Thread(target=handle_client, args=(conn, address)).start()
         print('Ожидание следующего клиента')
 
